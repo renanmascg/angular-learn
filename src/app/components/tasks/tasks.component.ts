ec309496fdb604e31143fb81b2682453cb2c5d87
Renan Mascarenhas <renanmascg@gmail.com>
2022-10-12 21:41:23 -0300

    feat: Adiciona o update dos reminders dos items dentro da aplicação

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -23,4 +23,10 @@ export class TasksComponent implements OnInit {
       .subscribe(() => (this.tasks = this.tasks.filter(t => t.id !== task.id)));
   }
 
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
 }
